62a1e14b73d45a68ed6048b08d3f53b4f8c4dddb
Vicente <165604764+mvr2004@users.noreply.github.com>
2024-06-29 15:31:52 +0100

    Delete routes directory

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/authRoutes.js
-const express = require('express');
-const { login, googleLogin } = require('../controllers/authController');
-const router = express.Router();
-
-router.post('/login', login);
-router.post('/login/google', googleLogin);
-
-module.exports = router;
--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/userRoutes.js
-const express = require('express');
-const { register, getData } = require('../controllers/userController');
-const router = express.Router();
-
-router.post('/register', register);
-router.get('/data', getData);
-
-module.exports = router;
