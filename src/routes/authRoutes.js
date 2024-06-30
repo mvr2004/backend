4a02c8a561a0213057e6761499ddeb5112e8577d
Vicente <165604764+mvr2004@users.noreply.github.com>
2024-06-30 11:44:03 +0100

    Add files via upload

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,20 @@
 // src/routes/authRoutes.js
 const express = require('express');
+const passport = require('passport');
 const { login, googleLogin } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/login/google', googleLogin);
+
+// Rotas para autenticação via Facebook
+router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+
+router.get('/facebook/callback', 
+  passport.authenticate('facebook', { failureRedirect: '/' }),
+  (req, res) => {
+    res.json({ message: 'Login bem-sucedido', user: req.user });
+  }
+);
 
 module.exports = router;
