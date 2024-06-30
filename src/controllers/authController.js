29491dbcf6490a165441f788848600238beb4314
Vicente <165604764+mvr2004@users.noreply.github.com>
2024-06-30 12:15:49 +0100

    Add files via upload

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -34,3 +34,35 @@ exports.googleLogin = async (req, res) => {
     res.status(400).json({ message: `Erro ao fazer login com Google: ${err.message}` });
   }
 };
+
+
+exports.facebookLogin = async (req, res) => {
+  const { accessToken, userData } = req.body;
+  console.log('Token recebido:', accessToken);
+
+  try {
+    // Verificar e registrar usuário com base no token do Facebook e dados do usuário
+    const user = await findOrCreateUserWithFacebook(accessToken, userData);
+    res.json({ message: 'Login bem-sucedido', user });
+  } catch (err) {
+    res.status(400).json({ message: `Erro ao fazer login com Facebook: ${err.message}` });
+  }
+};
+
+// Função auxiliar para lidar com login/registro de usuário via Facebook
+const findOrCreateUserWithFacebook = async (accessToken, userData) => {
+  const email = userData.email;
+  let user = await findUserByEmail(email);
+
+  if (!user) {
+    const password = Math.random().toString(36).slice(-8); // Gerar uma senha aleatória
+    const photoUrl = userData.picture.data.url;
+    const newUser = await registerUser(`${userData.first_name} ${userData.last_name}`, email, password, photoUrl);
+    if (!newUser) {
+      throw new Error('Falha ao registrar usuário');
+    }
+    user = await findUserByEmail(email);
+  }
+
+  return user;
+};
