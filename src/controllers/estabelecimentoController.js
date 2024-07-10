7ec13176135c7c3a3da84fd085f228aeabf04d35
Vicente <165604764+mvr2004@users.noreply.github.com>
2024-07-10 18:37:59 +0100

    Add files via upload

--- src/controllers/estabelecimentoController.js
+++ src/controllers/estabelecimentoController.js
@@ -26,64 +26,73 @@ const createEstablishment = async (req, res, next) => {
       return res.status(400).json({ error: 'Já existe um estabelecimento com este nome ou localização.' });
     }
 
-    // Executa o middleware de upload de imagem
-    upload.single('foto')(req, res, async (err) => {
-      if (err) {
-        return res.status(400).json({ error: 'Erro ao enviar a imagem.' });
-      }
-
-      try {
-        // Verifica se foi enviada uma imagem
-        if (!req.file) {
-          return res.status(400).json({ error: 'É necessário enviar uma imagem.' });
-        }
-
-        // Redimensiona a imagem utilizando sharp
-        const resizedImage = await sharp(req.file.path)
-          .resize({ width: 300, height: 300 })
-          .toBuffer();
-
-        // Define o nome do arquivo e o caminho onde será salvo
-        const filename = `${Date.now()}-${req.file.originalname}`;
-        const filepath = path.join(__dirname, '../public/uploads/', filename);
-
-        // Salva a imagem redimensionada no sistema de arquivos
-        await sharp(resizedImage).toFile(filepath);
-        console.log(`Imagem salva em ${filepath}`);
-
-        // Remove o arquivo temporário enviado pelo cliente
-        fs.unlink(req.file.path, (err) => {
+    // Função para processar o upload de imagem
+    const uploadImage = () => {
+      return new Promise((resolve, reject) => {
+        upload.single('foto')(req, res, async (err) => {
           if (err) {
-            console.error('Erro ao remover o arquivo temporário:', err);
+            console.error('Erro ao enviar a imagem:', err);
+            reject(err);
           } else {
-            console.log('Arquivo temporário removido com sucesso');
+            resolve();
           }
         });
+      });
+    };
 
-        // Cria o novo estabelecimento no banco de dados
-        const establishment = await Estabelecimento.create({
-          nome,
-          localizacao,
-          contacto,
-          descricao,
-          pago,
-          foto: `https://backend-9hij.onrender.com/uploads/${filename}`,
-          subareaId,
-          centroId
-        });
+    // Executa o middleware de upload de imagem
+    if (req.file) {
+      await uploadImage();
+    }
 
-        // Retorna o estabelecimento criado como resposta
-        res.status(201).json({ establishment });
-      } catch (imageError) {
-        console.error('Erro ao processar a imagem:', imageError);
-        return res.status(400).json({ error: 'Imagem inválida.' });
-      }
+    // Verifica se foi enviada uma imagem
+    let fotoUrl;
+    if (req.file) {
+      // Redimensiona a imagem utilizando sharp
+      const resizedImage = await sharp(req.file.path)
+        .resize({ width: 300, height: 300 })
+        .toBuffer();
+
+      // Define o nome do arquivo e o caminho onde será salvo
+      const filename = `${Date.now()}-${req.file.originalname}`;
+      const filepath = path.join(__dirname, '../public/uploads/', filename);
+
+      // Salva a imagem redimensionada no sistema de arquivos
+      await sharp(resizedImage).toFile(filepath);
+      console.log(`Imagem salva em ${filepath}`);
+
+      // Remove o arquivo temporário enviado pelo cliente
+      fs.unlink(req.file.path, (err) => {
+        if (err) {
+          console.error('Erro ao remover o arquivo temporário:', err);
+        } else {
+          console.log('Arquivo temporário removido com sucesso');
+        }
+      });
+
+      fotoUrl = `https://backend-9hij.onrender.com/uploads/${filename}`;
+    }
+
+    // Cria o novo estabelecimento no banco de dados
+    const establishment = await Estabelecimento.create({
+      nome,
+      localizacao,
+      contacto,
+      descricao,
+      pago,
+      foto: fotoUrl, // Salva a URL da foto no banco de dados
+      subareaId,
+      centroId
     });
+
+    // Retorna o estabelecimento criado como resposta
+    res.status(201).json({ establishment });
   } catch (error) {
     console.error('Erro ao criar o estabelecimento:', error);
     next(error); // Passa o erro para o próximo middleware de tratamento de erro
   }
 };
+
 
 // Controlador para buscar todos os estabelecimentos
 const getAllEstablishments = async (req, res, next) => {
