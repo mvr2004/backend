48d1ac73e5e129c38f8890ae1c874e9d20880865
Vicente <165604764+mvr2004@users.noreply.github.com>
2024-07-11 00:07:04 +0100

    Add files via upload

--- src/routes/estabelecimentoRoutes.js
+++ src/routes/estabelecimentoRoutes.js
@@ -1,7 +1,6 @@
 // src/routes/estabelecimentoRoutes.js
 const express = require('express');
 const estabelecimentoController = require('../controllers/estabelecimentoController'); 
-const avaliacaoEstabelecimentoController = require('../controllers/avaliacaoEstabelecimentoController');
 const router = express.Router();
 const upload = require('../config/uploadConfig');
 
@@ -24,28 +23,13 @@ router.get('/estab/:id', estabelecimentoController.getEstablishmentById);
 
 
 // Rota para criar uma avaliação de estabelecimento
-router.post('/avaliacao', avaliacaoEstabelecimentoController.createEstabelecimentoReview);
+router.post('/avaliacao', estabelecimentoController.createEstabelecimentoReview);
 
 // Rota para listar as avaliações de um estabelecimento
-router.get('/avaliacao/:estabelecimentoId', avaliacaoEstabelecimentoController.listEstabelecimentoReviews);
+router.get('/avaliacao/:estabelecimentoId', estabelecimentoController.listEstabelecimentoReviews);
 
 // Rota para calcular a média das avaliações de um estabelecimento
-router.get('/avaliacao/media/:estabelecimentoId', avaliacaoEstabelecimentoController.calculateEstabelecimentoAverageRating);
-
-app.get('/test/establishment/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const estabelecimento = await Estabelecimento.findByPk(id);
-    if (!estabelecimento) {
-      return res.status(404).json({ error: 'Estabelecimento não encontrado.' });
-    }
-    res.json({ estabelecimento });
-  } catch (error) {
-    console.error('Erro ao buscar estabelecimento por ID:', error);
-    res.status(500).json({ error: 'Erro interno do servidor.' });
-  }
-});
-
+router.get('/avaliacao/media/:estabelecimentoId', estabelecimentoController.calculateEstabelecimentoAverageRating);
 
 
 
