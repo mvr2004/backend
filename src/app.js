f4cb5d5bf03ad843b8155b35727af47dc2a7c061
Vicente <165604764+mvr2004@users.noreply.github.com>
2024-07-10 17:18:11 +0100

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
