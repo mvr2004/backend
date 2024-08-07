1264be3fde3afd95b31a69cf68cac4ddf0b3702a
Rapizzaaa <165604764+mvr2004@users.noreply.github.com>
2024-08-07 22:46:02 +0100

    Delete src directory

--- src/routes/centroRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/centroRoutes.js
-const express = require('express');
-const router = express.Router();
-const { listarCentros } = require('../controllers/centroController');
-
-router.get('/', listarCentros);
-
-module.exports = router;
