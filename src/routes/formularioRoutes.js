9ea66f87a6be5eab2ebc8182fa1fff51e2427b1c
Rapizzaaa <165604764+mvr2004@users.noreply.github.com>
2024-08-07 22:47:07 +0100

    Add files via upload

---
+++ src/routes/formularioRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const formularioController = require('../controllers/formularioController');
+
+// Rotas de formulário
+router.post('/formularios', formularioController.createFormulario);
+router.put('/formularios/:formularioId/estado', formularioController.updateFormularioStatus);
+
+// Rotas de campo de formulário
+router.post('/formularios/:formularioId/campos', formularioController.createCampoFormulario);
+
+// Rotas de resposta ao formulário
+router.post('/formularios/respostas', formularioController.submitFormularioResponse);
+router.get('/formularios/:formularioId/respostas', formularioController.getResponsesByFormulario);
+
+// Rotas para obter formulários respondidos
+router.get('/utilizadores/:utilizadorId/formularios/respondidos', formularioController.getFormulariosRespondidos);
+router.get('/utilizadores/:utilizadorId/eventos/:eventoId/formularios/:formularioId/respostas', formularioController.getFormularioResponsesByUserAndEvent);
+
+// Nova rota para pesquisar formulários respondidos de um evento
+router.get('/utilizadores/:utilizadorId/eventos/:eventoId/formularios/respondidos', formularioController.getFormulariosRespondidosPorEvento);
+
+module.exports = router;
